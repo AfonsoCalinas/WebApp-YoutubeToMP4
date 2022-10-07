d66e904a876d5af9d9d6e495a90cdd3741c71395
Calinas <calinaspentas@gmail.com>
2022-10-07 14:01:02 +0100

    Now it prints the message that the user posts on the input field.

--- main.py
+++ main.py
@@ -13,17 +13,21 @@ def index():
         return render_template("index.html")
 
 # MP4
-@app.route("/mp4/")
+@app.route("/mp4/", methods=["POST", "GET"])
 def mp4():
-    return render_template("mp4.html")
+    if request.method == "POST":
+        link = request.form["youtubeLink"]
+        return redirect(url_for("showvideofirst", url=link))
+    else:
+        return render_template("mp4.html")
 
 #SHOW VIDEO FIRST
-@app.route("/mp4/showvideofirst/")
-def showvideofirst():
+@app.route("/showvideofirst/<url>/")
+def showvideofirst(url):
     # link = request.args['youtubeLink']
     # yt = pytube.YouTube(link)
     # title = yt.title
-    return render_template("showvideofirst.html")
+    return render_template("showvideofirst.html", content=url)
 
 
 @app.route("/<url>")
